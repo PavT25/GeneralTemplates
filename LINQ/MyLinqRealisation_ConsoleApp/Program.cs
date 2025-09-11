992d7c6fc524cfcba01059cb00a306572806bfb4
PavT25 <myemail@mail>
2025-09-11 13:38:11 +0300

    unusefull comments were deleted

--- LINQ/MyLinqRealisation_ConsoleApp/Program.cs
+++ LINQ/MyLinqRealisation_ConsoleApp/Program.cs
@@ -1,6 +1,5 @@
 ﻿// See https://aka.ms/new-console-template for more information
 using MyLinqRealisation_ConsoleApp;
-using Enumerable = MyLinqRealisation_ConsoleApp.Enumerable;
 
 Console.WriteLine("--- Hello, World! ---");
 
@@ -11,11 +10,6 @@ List<Student> students = new List<Student>
     new Student { Id = 3, Name = "Bob", Age = 22 }
 };
 
-// Filter is another name of Where function
-//var filteredStudents = students.Filter(student => student.Age > 18);
-//var filteredStudents = students.Filter(student => student.Age > 18).Filter(student => student.Name.Equals("Bob"));
-
-//var filteredStudents = Enumerable.Where(Enumerable.Where(students, student => student.Age > 18), student => student.Name.Equals("Bob"));
 var filteredStudents = students.MyWhere(student => student.Age > 18).MyWhere(student => student.Name.Equals("Bob"));
 
 foreach (var student in filteredStudents)
