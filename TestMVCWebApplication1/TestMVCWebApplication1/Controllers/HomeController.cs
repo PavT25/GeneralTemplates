7242155da6dba09ce82bc444368d469431231d1c
PavT25 <myemail@mail>
2025-11-13 07:02:27 +0200

    TestMVCWebApplication1 was added

---
+++ TestMVCWebApplication1/TestMVCWebApplication1/Controllers/HomeController.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Web.Mvc;
+
+namespace TestMVCWebApplication1.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+
+        [HttpGet]
+        public JsonResult TestAction()
+        {
+            return Json(new { date = DateTime.Now.ToLongDateString(),  time = DateTime.Now.ToLongTimeString(), Data = "AAAA" }, "application/json", System.Text.Encoding.UTF8, JsonRequestBehavior.AllowGet);
+        }
+
+        [HttpGet]
+        public JsonResult TestAction2(string inputData)
+        {
+            return Json(new { date = DateTime.Now.ToLongDateString(), time = DateTime.Now.ToLongTimeString(), Data = inputData }, "application/json", System.Text.Encoding.UTF8, JsonRequestBehavior.AllowGet);
+        }
+
+
+
+
+
+    }
+}
---
+++ TestMVCWebApplication1/TestMVCWebApplication1/Scripts/Home/home.js
@@ -0,0 +1,63 @@
+
+$(document).ready(function () {
+    $("#testButton").on("click", function () {
+
+        console.log("testButton was clicked.");
+
+        const inputString = $("#inputData").val();
+        //alert("Input data: " + inputString);
+
+
+        ////works fine
+        //var jqxhr = $.get("Home/TestAction", function () {
+        //    console.log("* success *");
+        //})
+        //    .done(function (resultData) {
+        //        console.log("** second success **");
+
+        //        varresultString = "Result: \n";
+        //        varresultString += "DateTime: " + resultData.date + " " + resultData.time + ";\nData: " + resultData.Data + "\n";
+
+        //        var outputArea = $("#outputArea");
+        //        outputArea.val(outputArea.val() + varresultString);
+        //})
+        //    .fail(function () {
+        //        console.error("- error -");
+        //})
+        //    .always(function () {
+        //        console.info("_ finished _");
+        //});
+
+        //// Set another completion function for the request above
+        //jqxhr.always(function () {
+        //    console.info("__ second finished __");
+        //});
+
+        $.ajax({
+            type: "GET",
+            url: "Home/TestAction2/",
+            contentType: 'application/json; charset=utf-8',
+            data: { inputData: inputString },
+            cashe: false,
+            datatype: "text",
+            success: function (data) {
+                console.log('Submission was successful.');
+                console.log(data);
+
+                varresultString = "Result: \n";
+                varresultString += "DateTime: " + data.date + " " + data.time + ";\nData: " + data.Data + "\n";
+
+                var outputArea = $("#outputArea");
+                outputArea.val(outputArea.val() + varresultString);
+            },
+            error: function (data) {
+                console.log('An error occurred.');
+                console.log(data);
+            },
+        });
+    });
+
+
+
+});
+
